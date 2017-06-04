8282c12469d53eb704c0080cea6f66daa43a0559
棒棒勝 <jayuiop@gmail.com>
2017-06-04 20:32:14 +0800

    將 RTF.css 和 style.css 合併

--- header.php
+++ header.php
@@ -19,7 +19,6 @@
 
 		<!-- CSS -->
 		<link href="<?php bloginfo('stylesheet_url'); ?>" rel="stylesheet" type="text/css" media="screen" />
-		<link href="<?php bloginfo('template_url'); ?>/RTF.css" rel="stylesheet" type="text/css" media="screen" />
 		<link href="<?php bloginfo('template_url'); ?>/tocas-ui/tocas.min.css" rel="stylesheet" type="text/css" media="screen" />
 		<!-- /CSS -->
 
