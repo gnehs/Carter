6c637a2858457eccbb54df0a2f4b15d21fb82380
棒棒勝 <jayuiop@gmail.com>
2017-06-04 19:50:53 +0800

    移除編輯按鈕的判斷式，因為編輯按鈕本身就富有判斷式了

--- index.php
+++ index.php
@@ -24,16 +24,10 @@
 					<?php the_excerpt(); ?>
 				</div>
 			</div>
-			<?php if ( !is_user_logged_in() ) : ?>
-				<div class="ts fluid bottom attached buttons post operation">
-					<a class="ts labeled icon button click load" href="<?php the_permalink(); ?>"><i class="unhide icon"></i>繼續閱讀</a>
-				</div>
-       		<?php else : ?>
-				<div class="ts fluid bottom attached buttons post operation">
-					<a class="ts labeled icon button click load" href="<?php the_permalink(); ?>"><i class="unhide icon"></i>繼續閱讀</a>
-					<?php edit_post_link('<i class="write icon"></i>編輯', ''); ?>
-				</div>
-    		<?php endif; ?>
+			<div class="ts fluid bottom attached buttons post operation">
+				<a class="ts labeled icon button click load" href="<?php the_permalink(); ?>"><i class="unhide icon"></i>繼續閱讀</a>
+				<?php edit_post_link('<i class="write icon"></i>編輯', ''); ?>
+			</div>
 		</div>
 		<?php endwhile; ?>
 		</div>
