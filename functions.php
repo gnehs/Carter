ac0095aac9194319cc500eb4972684dba6cc9e23
棒棒勝 <jayuiop@gmail.com>
2017-06-12 11:57:47 +0800

    完善翻譯功能

--- functions.php
+++ functions.php
@@ -52,8 +52,8 @@ function aurelius_comment($comment, $args, $depth)
 					<?php comment_text(); ?>
 				</div>
 				<div class="actions">
-					<?php comment_reply_link(array_merge( $args, array('reply_text' => '回復','depth' => $depth, 'max_depth' => $args['max_depth']))) ?> 
-					<?php edit_comment_link('修改'); ?>
+					<?php comment_reply_link(array_merge( $args, array('reply_text' => __( '回復', 'Carter' ),'depth' => $depth, 'max_depth' => $args['max_depth']))) ?> 
+					<?php edit_comment_link(__( '修改', 'Carter' )); ?>
 				</div>
 			</div>
 		</div>
@@ -76,7 +76,7 @@ function update_today(){
     ),'ignore_sticky_posts' => 1);
     $postslist = get_posts( $args );
     if($postslist){
-        echo '<div class="ts card"><div class="content"><div class="header">本日更新</div><div class="description">今日更新 '. count($postslist) . ' 個項目，<a href="' . home_url('/').date('Y/m/d') . '">查閱</a>。</div></div></div>';
+        echo '<div class="ts card"><div class="content"><div class="header">'.__( '本日更新', 'Carter' ).'</div><div class="description">'.__( '本日更新', 'Carter' ).' '. count($postslist) .__( ' 個項目，', 'Carter' ). '<a href="' . home_url('/').date('Y/m/d') . '">'.__( '繼續閱讀', 'Carter' ).'</a>。</div></div></div>';
     }else{
         return false;
     }
@@ -88,9 +88,9 @@ function update_today(){
 add_action( 'widgets_init', 'register_Right_sidebar' );
 function register_Right_sidebar() {
     register_sidebar( array(
-       	'name' => '側邊欄',
+       	'name' => __( '側邊欄', 'Carter' ),
 		'id' => 'sidebar',
-		'description' => '顯示於每個網頁的右方。',
+		'description' => __( '顯示於每個網頁的右方。', 'Carter' ),
 		'before_widget' => '<div class="ts segment sidebarowo">',
 		'after_widget' => '</div>',
 		'before_title' => '<h5 class="ts header" style="margin-bottom: 5px;">',
@@ -102,8 +102,8 @@ function register_Right_sidebar() {
 	註冊選單
 */
 register_nav_menus( 
-	array('headernav' => __( '頁首選單'),
-		  'footernav' => __( '頁尾選單')
+	array('headernav' => __( '頁首選單', 'Carter' ),
+		  'footernav' => __(  '頁尾選單', 'Carter' )
          ) 
 );
 
