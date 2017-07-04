f037f6daec3294c8f21f8d753b088d72c0d6f9a6
棒棒勝 <jayuiop@gmail.com>
2017-07-04 21:19:44 +0800

    修正日曆沒有在底部欄呈現 100% 寬度的情形、讓頁面的標題跟文章的一樣

--- page.php
+++ page.php
@@ -3,18 +3,17 @@
 	<div id="content" class="ts narrow container" style="padding-top: 20px;"><div class="ts stackable grid">
 		<?php if (have_posts()) : the_post(); update_post_caches($posts); ?>
 		<div class="twelve wide column">
-			<div class="ts vertically very padded left aligned slate post">
+			<div class="ts heading vertically padded slate post">
 				<div class="image">
 					<?php if ( has_post_thumbnail() ) { ?>  
-						<?php the_post_thumbnail(); ?>
+						<?php the_post_thumbnail(); ?>  
 					<?php } else {?>  
 						<script src="//gnehs.github.io/RandomPic/randompicture.js"></script>  
 					<?php } ?> 
 				</div>
 				<span class="header"><?php the_title(); ?></span>
-				<span class="description"><?php the_time('Y/n/j') ?>  <a><?php edit_post_link( __( 'Edit', 'Carter' ), ''); ?></a></span>
+				<span class="description"><?php the_time('Y/n/j') ?> <?php edit_post_link(__( 'Edit', 'Carter' ), ''); ?></span>
 			</div>
-			
 			<div class="ts hidden divider"></div>
 			<post>
 				<?php the_content(); ?>
