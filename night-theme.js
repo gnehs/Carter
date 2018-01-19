5a55fcafbc02a41e8f6fa2a2ce2bafe94c2b9ec6
GNEHS <jayuiop@gmail.com>
2018-01-20 01:15:27 +0800

    調整程式碼位置

--- night-theme.js
+++ night-theme.js
@@ -1,3 +1,24 @@
+function NightMode(mode) {
+    $("body").attr("style", "");
+    var n = new Date().getHours();
+    var nightmode = window.localStorage["carternightmode"]
+    if (nightmode == "true") {
+        NightMode_switchToNightTheme();
+    }
+    if (!mode) {
+        return;
+    }
+    if (nightmode == "false") {
+        NightMode_switchToDayTheme();
+    }
+    if (mode && n > 20 && n < 6) {
+        NightMode_switchToNightTheme();
+    }
+    if (mode == "enabled") {
+        NightMode_switchToNightTheme(true);
+    }
+}
+
 function NightMode_switchToNightTheme(owo) {
     console.log("切換至暗色主題");
     NightMode_Changeing()
@@ -30,25 +51,4 @@ function NightMode_Changeing() {
     setTimeout(function() {
         $("[data-dark]").removeClass("changeing");
     }, 400);
-}
-
-function NightMode(mode) {
-    $("body").attr("style", "");
-    var n = new Date().getHours();
-    var nightmode = window.localStorage["carternightmode"]
-    if (nightmode == "true") {
-        NightMode_switchToNightTheme();
-    }
-    if (!mode) {
-        return;
-    }
-    if (nightmode == "false") {
-        NightMode_switchToDayTheme();
-    }
-    if (mode && n > 20 && n < 6) {
-        NightMode_switchToNightTheme();
-    }
-    if (mode == "enabled") {
-        NightMode_switchToNightTheme(true);
-    }
 }
