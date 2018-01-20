867cafc50486efe81098cd701dc96419706e7206
GNEHS <jayuiop@gmail.com>
2018-01-20 18:52:27 +0800

    修正底部的 NAV 夜間模式沒變樣式的問題

--- footer.php
+++ footer.php
@@ -8,7 +8,7 @@
 </div>
 <br>
 <!-- Footer -->
-		<nav class="ts basic fluid menu" data-dark>
+		<nav class="ts basic fluid menu" data-dark="basic">
 			<?php wp_nav_menu(
 				array(
 				'menu' => '', 
