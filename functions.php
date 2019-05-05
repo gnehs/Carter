6871d524e73fb9d3320c0bdd4fce0511796e6f80
gnehs <jayuiop@gmail.com>
2019-05-05 12:17:56 +0800

    Add files via upload

--- functions.php
+++ functions.php
@@ -183,4 +183,22 @@ function sig_add_editor_styles() {
     add_editor_style( 'editor-style.css' );
 }
 add_action( 'init', 'sig_add_editor_styles' );
+/*
+	隱藏版本
+*/
+// remove version from head
+remove_action('wp_head', 'wp_generator');
+
+// remove version from rss
+add_filter('the_generator', '__return_empty_string');
+
+// remove version from scripts and styles
+function shapeSpace_remove_version_scripts_styles($src) {
+	if (strpos($src, 'ver=')) {
+		$src = remove_query_arg('ver', $src);
+	}
+	return $src;
+}
+add_filter('style_loader_src', 'shapeSpace_remove_version_scripts_styles', 9999);
+add_filter('script_loader_src', 'shapeSpace_remove_version_scripts_styles', 9999);
 ?>
